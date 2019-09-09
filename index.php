54b29a2e82fff28bfaae0b22d00436cf23a9d837
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-09 11:53:07 +0300

    Pirma uzduotis v2

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 ?>
 
 <html>
+    <head>
+        <title>Aš valdau HTML!</title>
+    </head>
     <body>
-        <title>Aš valdau HTML</title>
-        <h1>Ignas - HTML asas</h1>
+        <h1>Ignas - HTML asas!</h1>
         <p>Moku kurti statinį turinį.</p>
     </body>
 </html>
