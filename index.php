2c7c121421d0e7e5b1ba8ab5a7cf59d505fa1af7
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-09 12:43:42 +0300

    Antra uzduotis 09-09 'tikrai perfect'

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ $data_laikas = date("Y/m/d h:i:s");
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Aš valdau HTML <?php print "ir PHP!"; ?></title>
+        <title>Aš valdau HTML <?php print 'ir PHP!'; ?></title>
     </head>
     <body>
-        <h1>Ignas - HTML <?php print "ir PHP"; ?> asas!</h1>
+        <h1>Ignas - HTML <?php print 'ir PHP'; ?> asas!</h1>
         <p>Kuriu dinaminį turinį. <br>
             Atidaryta: <?php print $data_laikas; ?>
         </p>
