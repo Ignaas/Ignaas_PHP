9917669d6f7fa073c5a56ae480fbf8a82e89c643
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-09 11:15:48 +0300

    Pirma uzduotis 09-09

--- index.php
+++ index.php
@@ -5,4 +5,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+?>
 
+<html>
+    <body>
+        <title>Aš valdau HTML</title>
+        <h1>Ignas - HTML asas</h1>
+        <p>Moku kurti statinį turinį.</p>
+    </body>
+</html>
+        
