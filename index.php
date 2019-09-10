2acaa95d24e6fc3bf8d3de279e6be2dd0ca0f876
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-10 15:23:25 +0300

    Komitas per Netbeansą

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $moksluPradzia = strtotime('2019-07-04');
 <html>
     <head>
         <meta charset="UTF-8">
-        <title><?php print 'Aš, ' . strftime("%A") . ' ir PHP'; ?></title>
+        <title><?php print 'Aš, ' . strftime('%A') . ' ir PHP'; ?></title>
     </head>
     <body>
         <h1>
@@ -16,8 +16,8 @@ $moksluPradzia = strtotime('2019-07-04');
             <?php print date('Y', $moksluPradzia) . ' metų' ?>
         </h1>
         <p>
-            Viskas prasidėjo <?php print strftime("%m") . '<br>'; ?>
-            <?php print ' mėnesio, ' . strftime("%e") . ' dieną!' ?>
+            Viskas prasidėjo <?php print strftime('%m'); ?> <br>
+            <?php print ' mėnesio, ' . strftime('%e') . ' dieną!' ?>
         </p>
     </body>
 </html>
