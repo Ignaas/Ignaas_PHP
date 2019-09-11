c496754deedd713dc62e1cf64ecebdbf6234021d
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-11 12:55:46 +0300

    Užduotis X - dideja

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
+<?php $laikas = date('s'); ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Primityvios logikos bomba</title>
+        <style>
+            img {
+                height: 1<?php print $laikas;?>px; 
+                width: 1<?php print $laikas;?>px;
+            }
+        </style>
     </head>
     <body>
         <img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ffiles.gamebanana.com%2Fimg%2Fico%2Fsprays%2F4ea33068c0dcc.png&f=1&nofb=1"/>
         <p>
-            <?php print date('s'); ?>
+            <?php print $laikas; ?>
         </p>
     </body>
 </html>
