0e8f2793654f77d1e4a2da796041976354bd86f4
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-11 10:58:50 +0300

    Užduotis #7 - tobuliausia

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>PHP lydės ir
-            <?php print date('Y-m-d', strtotime('- ' . rand(1, 10) . ' year - ' . rand(1, 12) . ' month - ' . rand(1, 28) . ' day')); ?>!
+        <title>
+            PHP lydės ir
+            <?php print date('Y-m-d', strtotime('+ ' . rand(1, 21349213481) . ' minutes')); ?>!
         </title>
     </head>
     <body>
-        <h1>
-            Ignas - Galbūt turėsiu 
-            <?php print rand(1, 5); ?> 
-            vaikų (us)!
-        </h1>
+        <h1>Ignas - Galbūt turėsiu <?php print rand(1, 5); ?> vaikų (us)!</h1>
         <p>
-            D. Trump'as nebebus prezidentu: <br>
-            <?php print date('Y-m-d', strtotime('+ ' . rand(2, 10) . ' year')) ?>
+            D. Trump'as nebebus prezidentu:<br>
+            <?php print date('Y-m-d', strtotime('+ ' . rand(2, 10) . ' year')); ?>
         </p>
     </body>
 </html>
