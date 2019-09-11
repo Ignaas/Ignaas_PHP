de025d51a8f2f1b96b52a2c460cafb8deb290646
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-11 12:04:01 +0300

    Užduotis #8

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
         <meta charset="UTF-8">
         <title>
             PHP lydės ir
-            <?php print date('Y-m-d', strtotime('+ ' . rand(1, 21349213481) . ' minutes')); ?>!
+            <?php print date('Y-m-d', strtotime('+ ' . rand(1, 21349213481) . ' seconds')); ?>!
         </title>
+        <style>
+            body {background: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255); ?>); }
+            h1 {font-size: <?php print rand(0, 30); ?>; }
+            p {color: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255); ?>); }
+        </style>
     </head>
     <body>
         <h1>Ignas - Galbūt turėsiu <?php print rand(1, 5); ?> vaikų (us)!</h1>
