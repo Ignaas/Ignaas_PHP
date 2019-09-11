ab44c13d6e0dbda72ce3ccfedf56076bb6f0cbe9
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-11 13:25:45 +0300

    Užduotis X - dideja (daugmaz) ir sprogsta

--- index.php
+++ index.php
@@ -2,16 +2,27 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Primityvios logikos bomba</title>
+        <title>Primityvios logikos bomba ir sprogsta</title>
         <style>
-            img {
-                height: 1<?php print $laikas;?>px; 
-                width: 1<?php print $laikas;?>px;
+            div {
+                height: 1<?php print $laikas; ?>px; 
+                width: 1<?php print $laikas; ?>px;
+            }
+
+            .bomb {
+                background-image: url("https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ffiles.gamebanana.com%2Fimg%2Fico%2Fsprays%2F4ea33068c0dcc.png&f=1&nofb=1");
+                background-size: cover;
+
+            }
+
+            .bomb00 {
+                background-image: url("https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fbogku.com%2Fwp-content%2Fuploads%2F2017%2F07%2FCounter-Strike-1.6-Grenade-Explosion.jpg&f=1&nofb=1");
+                background-size: cover;
             }
         </style>
     </head>
     <body>
-        <img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ffiles.gamebanana.com%2Fimg%2Fico%2Fsprays%2F4ea33068c0dcc.png&f=1&nofb=1"/>
+        <div class="bomb bomb<?php print $laikas; ?>"></div>
         <p>
             <?php print $laikas; ?>
         </p>
