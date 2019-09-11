0051bb9d86befef94d67654e07a789e88143bf9b
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-11 12:49:08 +0300

    Užduotis X

--- index.php
+++ index.php
@@ -1,29 +1,12 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>
-            PHP lydės ir
-            <?php print date('Y-m-d', strtotime('+ ' . rand(1, 21349213481) . ' seconds')); ?>!
-        </title>
-        <style>
-            body {
-                background: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255); ?>); 
-            }
-
-            h1 {
-                font-size: <?php print rand(0, 30); ?>;
-            }
-
-            p {
-                color: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255); ?>);
-            }
-        </style>
+        <title>Primityvios logikos bomba</title>
     </head>
     <body>
-        <h1>Ignas - Galbūt turėsiu <?php print rand(1, 5); ?> vaikų (us)!</h1>
+        <img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ffiles.gamebanana.com%2Fimg%2Fico%2Fsprays%2F4ea33068c0dcc.png&f=1&nofb=1"/>
         <p>
-            D. Trump'as nebebus prezidentu:<br>
-            <?php print date('Y-m-d', strtotime('+ ' . rand(2, 10) . ' year')); ?>
+            <?php print date('s'); ?>
         </p>
     </body>
 </html>
