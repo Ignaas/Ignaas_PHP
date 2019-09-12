ed57290987c3153dab82c4b2faa711be7e7ae02d
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-12 12:04:21 +0300

    Užduotis #4

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
 
-$skola_paimta = rand(1, 100);
-$skola_atiduodama_100_200 = rand(101, 200);
-$skola_atiduodama_200_300 = rand(201, 300);
+$siukslines_turis = 40;
+$siuskliu_turis_per_d = 15;
+$max_kaupo_turis = rand($siukslines_turis, $siukslines_turis + rand(1, 15));
+$current_siuksles = 0;
+$available_days = 0;
+while ($current_siuksles <= $max_kaupo_turis) {
+    $current_siuksles += $siuskliu_turis_per_d;
+    $available_days++;
+}
+$deadline = date('Y-m-d', strtotime("+ $available_days day"));
 
 ?>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Kintamieji - pataisyti - čia užduoties name - NEREDUNDANT</title>
-</head>
-<body>
-    <div>
-        <h1>Skolos skaičiuoklė</h1>
-        <h2>Jei paėmei <?php print $skola_paimta; ?> eurų.</h2>
-        <h3>Su vienu kabančiu grąžinsi <?php print $skola_atiduodama_100_200; ?> eurų.</h3>
-        <h3>Su dviem kabančiais grąžinsi <?php print $skola_atiduodama_200_300; ?> eurų.</h3>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Užduotis #4</title>
+    </head>
+    <body>
+        Po <?php print $available_days; ?> dienų (<?php print $deadline; ?>) pirk gėlių ir <br>
+        šampano, jeigu nori išvengti <br>
+        konflikto.
+    </body>
 </html>
