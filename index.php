ea173e0423ec655fdb89c53986d3d80ceb3b4597
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-12 10:10:52 +0300

    Kintamieji - 4 vienodi

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <?php
-$string = 'stringas';
-$int = 5;
-$bool = true;
-$null = NULL;
+$src = 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fmedias.slash-paris.com%2Fmain_images%2Fimages%2F000%2F005%2F357%2FIMAGE-PENSEE-HD-1_original_original_large.jpg%3F1372235419&f=1&nofb=1'
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Kintamieji</title>
+        <title>Kintamieji - 4 vienodi</title>
     </head>
     <body>
-        <ul>
-            <li>String Tipas: <?php print $string; ?></li>
-            <li>Integer Tipas: <?php print $int; ?></li>
-            <li>Boolean Tipas: <?php print $bool; ?></li>
-            <li>Null Tipas: <?php print $null; ?></li>
-        </ul>
+         <img src="<?php print $src; ?>" /> 
+         <img src="<?php print $src; ?>" /> 
+         <img src="<?php print $src; ?>" /> 
+         <img src="<?php print $src; ?>" /> 
     </body>
 </html>
