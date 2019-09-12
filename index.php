c5a45ab73c38926428b53aa56d70f98ffe58d7a1
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-12 10:49:47 +0300

    Kintamieji - pataisymas vel

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$paimta_skola = (rand(1, 100));
-$atiduodama_skola_pirma = (rand(101, 200));
-$atiduodama_skola_antra = (rand(201, 300));
-$atiduodama_skola_trecia = (rand(301, 400));
+$skola_paimta = (rand(1, 100));
+$skola_atiduodama_extra100 = (rand(101, 200));
+$skola_atiduodama_extra200 = (rand(201, 300));
+$skola_atiduodama_extra300 = (rand(301, 400));
 
 ?>
 <head>
@@ -13,10 +13,10 @@ $atiduodama_skola_trecia = (rand(301, 400));
 <body>
     <div class="body-custom">
         <h1>Skolos skaičiuoklė</h1>
-        <h2>Jei paėmei <?php print $paimta_skola; ?> eurų</h2>
-        <h3>Su vienu kabančiu grąžinsi <?php print $atiduodama_skola_pirma; ?> eurų.</h3>
-        <h3>Su dviem kabančiais grąžinsi <?php print $atiduodama_skola_antra; ?> eurų.</h3>
-        <h3>Su trimis kabančiais grąžinsi <?php print $atiduodama_skola_trecia; ?> eurų.</h3>
+        <h2>Jei paėmei <?php print $paimta_skola; ?> eurų.</h2>
+        <h3>Su vienu kabančiu grąžinsi <?php print $skola_atiduodama_extra100; ?> eurų.</h3>
+        <h3>Su dviem kabančiais grąžinsi <?php print $skola_atiduodama_extra200; ?> eurų.</h3>
+        <h3>Su trimis kabančiais grąžinsi <?php print $skola_atiduodama_extra300; ?> eurų.</h3>
     </div>
 </body>
 </html>
