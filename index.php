e6350cffaf4cb3cd294426cc55f41ba7d36367a6
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-12 11:03:50 +0300

    Kintamieji - tobula

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 $skola_paimta = (rand(1, 100));
 $skola_atiduodama_extra100 = (rand(101, 200));
 $skola_atiduodama_extra200 = (rand(201, 300));
-$skola_atiduodama_extra300 = (rand(301, 400));
 
 ?>
 <head>
@@ -14,9 +13,8 @@ $skola_atiduodama_extra300 = (rand(301, 400));
     <div>
         <h1>Skolos skaičiuoklė</h1>
         <h2>Jei paėmei <?php print $skola_paimta; ?> eurų.</h2>
-        <h3>Su vienu kabančiu grąžinsi <?php print "$skola_paimta + $skola_atiduodama_extra100"; ?> eurų.</h3>
-        <h3>Su dviem kabančiais grąžinsi <?php print "$skola_paimta + $skola_atiduodama_extra200"; ?> eurų.</h3>
-        <h3>Su trimis kabančiais grąžinsi <?php print "$skola_paimta + $skola_atiduodama_extra300"; ?> eurų.</h3>
+        <h3>Su vienu kabančiu grąžinsi <?php print $skola_atiduodama_extra100; ?> eurų.</h3>
+        <h3>Su dviem kabančiais grąžinsi <?php print $skola_atiduodama_extra200; ?> eurų.</h3>
     </div>
 </body>
 </html>
