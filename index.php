2821b4f1427d7332d82071d8946b1ac9eaee5525
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-12 10:44:13 +0300

    Kintamieji - pataisymas

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
-$src = 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fmedias.slash-paris.com%2Fmain_images%2Fimages%2F000%2F005%2F357%2FIMAGE-PENSEE-HD-1_original_original_large.jpg%3F1372235419&f=1&nofb=1'
+
+$paimta_skola = (rand(1, 100));
+$atiduodama_skola_pirma = (rand(101, 200));
+$atiduodama_skola_antra = (rand(201, 300));
+$atiduodama_skola_trecia = (rand(301, 400));
+
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Kintamieji - 4 vienodi</title>
-    </head>
-    <body>
-         <img src="<?php print $src; ?>" /> 
-         <img src="<?php print $src; ?>" /> 
-         <img src="<?php print $src; ?>" /> 
-         <img src="<?php print $src; ?>" /> 
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>Kintamieji - pataisyti</title>
+</head>
+<body>
+    <div class="body-custom">
+        <h1>Skolos skaičiuoklė</h1>
+        <h2>Jei paėmei <?php print $paimta_skola; ?> eurų</h2>
+        <h3>Su vienu kabančiu grąžinsi <?php print $atiduodama_skola_pirma; ?> eurų.</h3>
+        <h3>Su dviem kabančiais grąžinsi <?php print $atiduodama_skola_antra; ?> eurų.</h3>
+        <h3>Su trimis kabančiais grąžinsi <?php print $atiduodama_skola_trecia; ?> eurų.</h3>
+    </div>
+</body>
 </html>
