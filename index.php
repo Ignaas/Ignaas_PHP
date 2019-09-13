72c76cb4ec2191fd3fee638a717435dd60d5a427
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-13 12:04:03 +0300

    Užduotis #4

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 $grizai_velai = rand(0, 1);
 $grizai_isgeres = rand(0, 1);
+$sofa = 'Nemiegosi ant sofos';
 
 $situacija = 'Situacija: ';
 if ($grizai_velai) {
@@ -9,6 +10,7 @@ if ($grizai_velai) {
 
     if ($grizai_isgeres) {
         $situacija .= ' ir išgėręs';
+        $sofa = 'Miegosi ant sofos';
     }
 } elseif ($grizai_isgeres) {
     $situacija .= 'Grįžai išgėręs';
@@ -20,10 +22,11 @@ if ($grizai_velai) {
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Užduotis #3</title>
+        <title>Užduotis #4</title>
     </head>
     <body>
         <h1>Buitinė skaičiuoklė</h1>
         <h2><?php print $situacija; ?></h2>
+        <h3><?php print $sofa; ?></h3>
     </body>
 </html>
