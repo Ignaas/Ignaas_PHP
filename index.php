cf2eb01c233308e91e29126a8436be9b2f17f5ba
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-16 12:59:38 +0300

    Užduotis #5

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <?php
 
-$months = 12;
-$wallet = 1000;
-$month_income = 700;
+$days = 365;
+$pack_price = 3.4;
+$count_ttl = 0;
 
-for ($i = 1; $i <= $months; $i++) {
-    $month_expenses = rand(100, 1500);
-    $wallet += $month_income - $month_expenses;
+for ($i = 1; $i <= $days; $i++) {
+    $cigs_mon_fri = rand(3, 4);
+    $cigs_sat = rand(10, 20);
+    $cigs_sun = rand(5, 6);
 
-    if ($wallet < 0) {
-        $h2_forecast = "Atsargiai, $i mėnesį gali baigtis pinigai!";
-        break;
+    $loop_date = date('N', $i);
+    if ($loop_date == 7) {
+        $count_ttl += $cigs_sun;
+    } elseif ($loop_date == 6) {
+        $count_ttl += $cigs_sat;
     } else {
-        $h2_forecast = "Po $months m., prognozuojamas likutis: $wallet";
+        $count_ttl += $cigs_mon_fri;
     }
 }
+
+$price_ttl = $count_ttl * $pack_price;
+$cig_maths = "Per $days dienas, surukysiu $count_ttl cigarečių už $price_ttl eur.";
 
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Užduotis #4</title>
+        <title>Užduotis #5</title>
     </head>
     <body>
-        <h1>Wallet forecast</h1>
-        <h2><?php print $h2_forecast; ?></h2>
+        <h1>Mano dūmų skaičiuoklė</h1>
+        <h2><?php print $cig_maths; ?></h2>
     </body>
 </html>
