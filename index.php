2a52c4776509f2413c51cdf8f2d536dceb8e4399
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-16 09:40:18 +0300

    Užduotis #1

--- index.php
+++ index.php
@@ -1,43 +1,17 @@
 <?php
 
-$orai = rand(0, 1);
+$x = rand(0, 500);
 
-if ($orai) {
-    $css_orai = 'sauleta';
-    $text_value = 'Saulėta';
-} else {
-    $css_orai = 'debesuota';
-    $text_value = 'Debesuota';
+for ($i = 0; $i < $x; $i++) {
+    print "Tai yra $i-tasis ciklas <br>";
 }
 
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Užduotis #5</title>
-        <style>
-            .orai_box {
-                height: 128px;
-                width: 128px;
-            }
-
-            .sauleta {
-                float: left;
-                background-image: url(https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ffiles.softicons.com%2Fdownload%2Ftoolbar-icons%2Fvista-weather-icons-by-icons-land%2Fpng%2F128x128%2FSunny.png&f=1&nofb=1);
-            }
-
-            .debesuota {
-                float: left;
-                background-image: url(https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fthegazette.com.au%2Fwp-content%2Fuploads%2Fweather_icons%2Fmostlycloudy.png&f=1&nofb=1);
-            }
-
-            .text {
-                padding-top: 50px;
-            }
-        </style>
+        <title>Užduotis #1</title>
     </head>
     <body>
-        <div class="orai_box <?php print $css_orai; ?>"></div>
-        <div class="text"><?php print $text_value; ?></div>
     </body>
 </html>
