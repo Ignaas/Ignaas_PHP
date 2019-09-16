7157f6abec36dcbe0c4feb9cd8b84fd30edd9366
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-16 15:49:25 +0300

    Užduotis #7 overkill

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
 $days = 365;
-$pack_price = 3.4;
 $count_ttl = 0;
 
 for ($i = 1; $i <= $days; $i++) {
-    $cigs_mon_fri = rand(3, 4);
-    $cigs_sat = rand(10, 20);
     $cigs_sun = rand(5, 6);
-    
+    $cigs_sat = rand(10, 20);
+    $cigs_mon_fri = rand(3, 4);
+    $time_per_cig = rand(3, 7);
+    $pack_price = rand(3.4, 3.6);
+
     $loop_date = date('N', strtotime("+$i day"));
     if ($loop_date == 7) {
         $count_ttl += $cigs_sun;
@@ -19,8 +20,23 @@ for ($i = 1; $i <= $days; $i++) {
     }
 }
 
-$price_ttl = ($count_ttl / 20) * $pack_price;
-$cigs_text = "Per $days dienas, surukysiu $count_ttl cigarečių už $price_ttl eur.";
+$time_total = $count_ttl * $time_per_cig / 60;
+$time_hours = intval($time_total);
+$time_mins = intval(($time_total - $time_hours) * 60);
+$price_ttl = (intval($count_ttl / 20) + 1) * $pack_price;
+
+$h2_text = "Per $days dienas, surukysiu $count_ttl cigarečių už $price_ttl eur.";
+$h3_text = "Viso traukdamas prastovėsiu $time_hours valandų ";
+
+if ($time_mins != 0) {
+    if ($time_mins > 0 && $time_mins < 10) {
+        $h3_text .= "ir $time_mins minutes.";
+    } else {
+        $h3_text .= "ir $time_mins minučių.";
+    }
+} else {
+    $h3_text = "Viso traukdamas prastovėsiu $time_hours valandų.";
+}
 
 ?>
 <html>
@@ -30,6 +46,7 @@ $cigs_text = "Per $days dienas, surukysiu $count_ttl cigarečių už $price_ttl 
     </head>
     <body>
         <h1>Mano dūmų skaičiuoklė</h1>
-        <h2><?php print $cigs_text; ?></h2>
+        <h2><?php print $h2_text; ?></h2>
+        <h3><?php print $h3_text; ?></h3>
     </body>
 </html>
