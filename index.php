8da2fb8c87c4d016466e8c700eeb943724cd6e12
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-16 12:30:25 +0300

    Užduotis #4

--- index.php
+++ index.php
@@ -4,16 +4,23 @@ $months = 12;
 $wallet = 1000;
 $month_income = 700;
 
-for ($i = 0; $i < $months; $i++) {
+for ($i = 1; $i <= $months; $i++) {
     $month_expenses = rand(100, 2000);
     $wallet += $month_income - $month_expenses;
+
     if ($wallet < 0) {
-        print "Atsargiai, $i mėnesį gali baigtis pinigai!";
+        $error_forecast = "Atsargiai, $i mėnesį gali baigtis pinigai!";
         break;
+    } else {
+        $error_forecast = '';
     }
 }
 
-$h2_forecast = "Po $months m., prognozuojamas likutis: $wallet";
+if ($error_forecast != '') {
+    $h2_forecast = $error_forecast;
+} else {
+    $h2_forecast = "Po $months m., prognozuojamas likutis: $wallet";
+}
 
 ?>
 <html>
