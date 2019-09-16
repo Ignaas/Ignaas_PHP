7a6aef7fdf01428e8cde3960ac5fd1c8fa74f851
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-16 10:23:07 +0300

    Užduotis #2

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
 
-$x = rand(0, 500);
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$pavyko = rand(0, 1);
+$katasuniai = 0;
 
-for ($i = 0; $i < $x; $i++) {
-    print "Tai yra $i-tasis ciklas <br>";
+for ($i = 0; $i < $sunys; $i++) {
+    for ($j = 0; $j < $kates; $j++) {
+        if ($pavyko) {
+            $katasuniai++;
+            break;
+        }
+    }
 }
 
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Užduotis #1</title>
+        <title>Užduotis #2</title>
     </head>
     <body>
+        <h1>Katašunių Išeiga</h1>
+        <h2>Dalyvavo <?php print $kates; ?> katės ir <?php print $sunys; ?> šunys</h2>
+        <h3>Katašunių išeiga: <?php print $katasuniai ?></h3>
     </body>
 </html>
