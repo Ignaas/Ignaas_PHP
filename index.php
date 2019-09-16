2afaf92e81116a0504629b9daa92bae715fb47e6
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-16 12:17:54 +0300

    Užduotis #4

--- index.php
+++ index.php
@@ -6,8 +6,11 @@ $month_income = 700;
 
 for ($i = 0; $i < $months; $i++) {
     $month_expenses = rand(100, 2000);
-    $wallet += $month_income;
-    $wallet -= $month_expenses;
+    $wallet += $month_income - $month_expenses;
+    if ($wallet < 0) {
+        print "Atsargiai, $i mėnesį gali baigtis pinigai!";
+        break;
+    }
 }
 
 $h2_forecast = "Po $months m., prognozuojamas likutis: $wallet";
@@ -16,7 +19,7 @@ $h2_forecast = "Po $months m., prognozuojamas likutis: $wallet";
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Užduotis #3</title>
+        <title>Užduotis #4</title>
     </head>
     <body>
         <h1>Wallet forecast</h1>
