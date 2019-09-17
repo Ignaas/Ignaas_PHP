08eb2bae7f3521b5f6cf338f1e62359f76fd9e6d
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-17 12:05:46 +0300

    Užduotis #2

--- index.php
+++ index.php
@@ -7,10 +7,11 @@ $depreciation = 2;
 $depreciation_absolute = (100 - $depreciation) / 100;
 
 for ($i = 1; $i <= $months; $i++) {
-    intval($car_price_used *= $depreciation_absolute);
+    $car_price_used *= $depreciation_absolute;
 }
 
 $depr_perc = intval((1 - $car_price_used / $car_price_new) * 100);
+$car_price_used = intval($car_price_used);
 
 $h2_text = "Naujos mašinos kaina: $car_price_new";
 $h3_text = "Po $months mėn, mašinos vertė bus: $car_price_used eur.";
