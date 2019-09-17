31f23b41c48074e5cf47227849c70f43fb328b19
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-17 12:41:25 +0300

    Šabla

--- index.php
+++ index.php
@@ -1,30 +1,25 @@
 <?php
 
-$months = 24;
-$car_price_new = 30000;
-$car_price_used = $car_price_new;
-$depreciation = 2;
-$depreciation_absolute = (100 - $depreciation) / 100;
+$aa = 88;
 
-for ($i = 1; $i <= $months; $i++) {
-    $car_price_used *= $depreciation_absolute;
+for ($i = 88; $i <= 88; $i++) {
+    
 }
 
-$depr_perc = intval((1 - $car_price_used / $car_price_new) * 100);
-$car_price_used_int = intval($car_price_used);
+$depr_perc = 88;
 
-$h2_text = "Naujos mašinos kaina: $car_price_new";
-$h3_text = "Po $months mėn, mašinos vertė bus: $car_price_used_int eur.";
-$h4_text = "Mašina nuvertės $depr_perc proc.";
+$h2_text = " ";
+$h3_text = " ";
+$h4_text = " ";
 
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Užduotis #2</title>
+        <title>Užduotis #3</title>
     </head>
     <body>
-        <h1>Kiek nuvertės mašina?</h1>
+        <h1>texttext?</h1>
         <h2><?php print $h2_text; ?></h2>
         <h3><?php print $h3_text; ?></h3>
         <h4><?php print $h4_text; ?></h4>
