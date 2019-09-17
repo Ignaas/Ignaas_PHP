c2d24024dd839925f6e1c0b8be031236947af6e4
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-17 11:59:51 +0300

    Užduotis #2

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
 
-$grikiai = 5000;
-$grikiai_init = $grikiai;
-$days = 0;
+$months = 24;
+$car_price_new = 30000;
+$car_price_used = $car_price_new;
+$depreciation = 2;
+$depreciation_absolute = (100 - $depreciation) / 100;
 
-for ($per_day = rand(200, 550); $grikiai > 0; $days++) {
-    if ($grikiai < $per_day) {
-        break;
-    } else {
-        $grikiai -= $per_day;
-    }
+for ($i = 1; $i <= $months; $i++) {
+    intval($car_price_used *= $depreciation_absolute);
 }
 
-$data = date('Y-m-d', strtotime("+ $days day"));
+$depr_perc = intval((1 - $car_price_used / $car_price_new) * 100);
 
-$h2_text = "Rasta grikių: $grikiai_init g.";
-$h3_text = "Išgyvensiu dar $days dienas, iki $data.";
+$h2_text = "Naujos mašinos kaina: $car_price_new";
+$h3_text = "Po $months mėn, mašinos vertė bus: $car_price_used eur.";
+$h4_text = "Mašina nuvertės $depr_perc proc.";
 
 ?>
 <html>
@@ -24,8 +23,9 @@ $h3_text = "Išgyvensiu dar $days dienas, iki $data.";
         <title>Užduotis #1</title>
     </head>
     <body>
-        <h1>Kiek dienų galėsi valgyti grikius?</h1>
+        <h1>Kiek nuvertės mašina?</h1>
         <h2><?php print $h2_text; ?></h2>
         <h3><?php print $h3_text; ?></h3>
+        <h4><?php print $h4_text; ?></h4>
     </body>
 </html>
