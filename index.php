9fda3a4be428637856864191a68adeed978d8aee
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-17 13:03:12 +0300

    Užduotis #3

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
 
-$aa = 88;
+$santaupos = 15000;
+$months = 48;
+$car_price_new = 30000;
+$car_price_used = $car_price_new;
+$depreciation = 2;
+$depreciation_absolute = (100 - $depreciation) / 100;
 
-for ($i = 88; $i <= 88; $i++) {
-    
+
+for ($i = 1; $i <= $months; $i++) {
+    $car_price_used *= $depreciation_absolute;
+
+    if ($car_price_used <= $santaupos) {
+        $months = $i;
+        break;
+    }
 }
 
-$depr_perc = 88;
+$depr_perc = intval((1 - $car_price_used / $car_price_new) * 100);
+$car_price_used_int = intval($car_price_used);
 
-$h2_text = " ";
-$h3_text = " ";
-$h4_text = " ";
+$h2_text = "Naujos mašinos kaina: $car_price_new";
+$h3_text = "Mašiną galėsi nusipirkti po $months mėn, kai jos vertė bus: $car_price_used_int eur.";
+$h4_text = "Mašina nuvertės $depr_perc proc.";
 
 ?>
 <html>
@@ -19,7 +31,7 @@ $h4_text = " ";
         <title>Užduotis #3</title>
     </head>
     <body>
-        <h1>texttext?</h1>
+        <h1>Kiek nuvertės mašina?</h1>
         <h2><?php print $h2_text; ?></h2>
         <h3><?php print $h3_text; ?></h3>
         <h4><?php print $h4_text; ?></h4>
