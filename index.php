564530506169e87d9bbeb08098d43f4d9712a4cb
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-18 12:49:15 +0300

    Užduotis #2

--- index.php
+++ index.php
@@ -1,22 +1,32 @@
 <?php
 
-$mano_atmintis = ['Ey', 'Skomantas', 'Baras', 'Gvidas Ojeras', 'Gudeliunas', 'vegetarian', 'legislature', 'temple'];
+$mano_atmintis = [
+    'Ey', 'Skomantas', 'Baras',
+    'Gvidas Ojeras', 'Gudeliunas', 'vegetarian',
+    'legislature', 'temple'
+];
+
+$count = count($mano_atmintis);
+$id_random = rand(0, $count);
+$rand_flashback = $mano_atmintis[$id_random];
+$id_random++;
+
+$h3_text = "Flashback $id_random: $rand_flashback";
 
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Užduotis #1</title>
+        <title>Užduotis #2</title>
     </head>
     <body>
         <h1>Kas buvo penktadienį?</h1>
         <h2>Ignas artimesnis atmintis</h2>
         <ul>
             <?php foreach ($mano_atmintis as $mintis): ?>
-            <li>
-                <?php print $mintis; ?>
-            </li>
+                <li><?php print $mintis; ?></li>
             <?php endforeach; ?>
         </ul>
+        <h3><?php print $h3_text; ?></h3>
     </body>
 </html>
