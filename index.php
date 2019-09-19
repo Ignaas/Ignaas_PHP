8a4f3a7b6e37deb188de7dede0f5a86d8c693953
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-19 14:46:38 +0300

    Užduotis atmintis #3 - namų darbas

--- index.php
+++ index.php
@@ -1,18 +1,43 @@
 <?php
 
-$days = [
-    1 => 'Pirmadienis',
-    2 => 'Antradienis',
-    3 => 'Trečiadienis',
-    4 => 'Ketvirtadienis',
-    5 => 'Penktadienis',
-    6 => 'Šeštadienis',
-    7 => 'Sekmadienis',
+$mano_atmintis = [
+    'Ey', 'Skomantas', 'Baras',
+    'Gvidas Ojeras', 'Gudeliunas', 'vegetarian',
+    'legislature', 'temple',
 ];
 
-$days[5] = 'Blackout';
-unset($days[5]);
+$draugo_atmintis = [
+    'We wasn\'t', 'supposed to', 'make it', 'past 25',
+    'legislature', 'Ey', 'Skomantas', 'boom', 'temple',
+];
 
-var_dump($days);
+$count = count($mano_atmintis) - 1;
+$id_random = rand(0, $count);
+$rand_flashback = $mano_atmintis[$id_random];
+$id_random++;
+
+$h3_text = "Flashback #$id_random: $rand_flashback";
 
 ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Užduotis #3</title>
+    </head>
+    <body>
+        <h1>Kas buvo penktadienį?</h1>
+        <h2>Ignas artimesnis atmintis</h2>
+        <ul>
+            <?php foreach ($mano_atmintis as $mintis): ?>
+                <li><?php print $mintis; ?></li>
+            <?php endforeach; ?>
+        </ul>
+        <h3><?php print $h3_text; ?></h3>
+        <h2>Draugo atmintis</h2>
+        <ul>
+            <?php foreach ($draugo_atmintis as $mintis): ?>
+                <li><?php print $mintis; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
+</html>
