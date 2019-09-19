1c2265889272b8dec45aa1fd0ab3deb7b2ad72f4
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-19 12:25:14 +0300

    Užduotis #1

--- index.php
+++ index.php
@@ -2,29 +2,33 @@
 
 $bank_report = [
     [
-        name => 'IKI darbo užmokestis',
-        amount => 600,
+        'name' => 'IKI darbo užmokestis',
+        'amount' => 600,
     ],
     [
-        name => 'Kalvarijų Načnykas',
-        amount => -15,
+        'name' => 'Kalvarijų Načnykas',
+        'amount' => -15,
     ],
     [
-        name => 'Pinigų išgryninimas Naugarduko g.',
-        amount => -10000,
+        'name' => 'Pinigų išgryninimas Naugarduko g.',
+        'amount' => -10000,
     ],
     [
-        name => 'Pinigų išgryninimo mokestis',
-        amount => -100000000,
+        'name' => 'Pinigų išgryninimo mokestis',
+        'amount' => -100000000,
     ],
 ];
 
-foreach ($bank_report as $report) {
-    if ($report['amount'] > 0) {
-        $report['css_class'] = 'income';
-    } else {
-        $report['css_class'] = 'expense';
+foreach ($bank_report as $key => $value) {
+    foreach ($value as $item) {
+        if ($value['amount'] > 0) {
+            $bank_report[$key]['css_class'] = 'income';
+        } else {
+            $bank_report[$key]['css_class'] = 'expense';
+        }
     }
 }
+
+var_dump($bank_report);
 
 ?>
