24a470dda51d0f1919a871591463c3549b4206bc
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-19 16:12:24 +0300

    Užduotis #5 - namų darbas

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
 
-$zodziai = [
-    'ey', 'skomantas', 'baras',
-    'Gvidas Ojeras', 'gudeliunas', 'vegetarian',
-    'legislature', 'temple',
+$daiktai = [
+    [
+        'name' => 'Kremas',
+        'size' => 20,
+    ],
+    [
+        'name' => 'Bananas',
+        'size' => 8,
+    ],
+    [
+        'name' => 'Piniginė',
+        'size' => 10,
+    ],
+    [
+        'name' => '20 e kupiūra',
+        'size' => 0.4,
+    ],
+    [
+        'name' => '50 e kupiūra',
+        'size' => 0.5,
+    ],
 ];
-$rasinys = '';
 
-for ($i = 0; $i <= rand(0, rand(1, 500)); $i++) {
-    $rasinys .= $zodziai[rand(0, count($zodziai) - 1)] . ' ';
+$rankinukas = [];
+
+for ($i = 0; $i <= rand(0, rand(1, rand(0, rand(1, 80)))); $i++) {
+    $rankinukas[] = $daiktai[rand(0, count($daiktai) - 1)];
 }
 
 ?>
@@ -18,8 +36,12 @@ for ($i = 0; $i <= rand(0, rand(1, 500)); $i++) {
         <title>PHP Array namu darbai</title>
     </head>
     <body>
-        <h1>Lietuvių Egzaminas</h1>
-        <p><?php print $rasinys; ?></p>
+        <h1>Ką moteris turi rankinuke?</h1>
+        <ul>
+            <?php foreach ($rankinukas as $daiktas): ?>
+                <li><?php print $daiktas['name'] . ' užima ' . $daiktas['size'] . 'cm3.'; ?></li>
+            <?php endforeach; ?>
+        </ul>
 
     </body>
 </html>
