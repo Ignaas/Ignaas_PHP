3e7860612a9a9952247d99d19ca3359ebd4c906a
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-25 16:07:15 +0300

    Užduotis #5

--- index.php
+++ index.php
@@ -11,6 +11,35 @@ function slot_run($size) {
     return $array;
 }
 
-$slots_machine = slot_run(rand(2, 6));
+$slots_machine = slot_run(rand(2, 4));
 
-var_dump($slots_machine);
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <style>
+            span {
+                display: inline-block;
+                height: 50px;
+                width: 50px;
+            }
+            .slots-col-win {
+                background: #ffc000;
+                border: 1px solid black;
+            }
+            .slots-col-loss {
+                background: #00a3da;
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+        <?php foreach ($slots_machine as $row_slots): ?>
+            <div>
+                <?php foreach ($row_slots as $col_slots): ?>
+                    <span class="slots-col-<?php print $col_slots ? 'win' : 'loss'; ?>"></span>
+                <?php endforeach; ?>
+            </div>
+        <?php endforeach; ?>
+    </body>
+</html>
