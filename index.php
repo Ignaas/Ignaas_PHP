1359bda0bb821b52e356189e442e088543bbec90
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-25 10:57:57 +0300

    Užduotis #2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $x = rand(0, 20);
 
 function is_prime($x) {
-    for ($i = 2; $i <= $x; $i++) {
+    for ($i = 2; $i < $x; $i++) {
         if ($x % $i == 0) {
             return false;
             break;
