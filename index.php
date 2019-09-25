f1a7e12e573186ef3af40b498232ce308ec28d0e
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-25 13:12:41 +0300

    Užduotis #4

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 function slot_run($size) {
+    $array = [];
+
     for ($i = 1; $i <= $size; $i++) {
         for ($j = 1; $j <= $size; $j++) {
             $array[$i][$j] = rand(0, 1);
@@ -12,5 +14,3 @@ function slot_run($size) {
 $slots = slot_run(rand(2, 6));
 
 var_dump($slots);
-
-?>
