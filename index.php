14c9ed73204df70545e5686957f1e121a5142730
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-30 09:55:30 +0300

    Užduotis #1 - gerai?

--- index.php
+++ index.php
@@ -1,27 +1,16 @@
 <?php
 
-$masyvas = [
-    'kiesa500' => 'stringinis',
-    1 => ['reiksme0', 'nemasyvas1', 'tekstas2', ['dar geriaurekursyvoblio']],
-];
-
-function print_array($array) {
-    $output_array = [];
-
-    foreach ($array as $keys => $values) {
-        if (is_array($values)) {
-            $output_array = array_merge($output_array, print_array($values));
-        } else {
-            $output_array[$keys] = $values;
-        }
-    }
-    return $output_array;
+function square($x) {
+    return $x ** 2;
 }
 
-print_array($masyvas);
-print '<br><br><br>';
-$viematmas = print_array($masyvas);
-var_dump($viematmas);
+$kvadratas = 0;
+
+if (isset($_POST['name'])) {
+    $kvadratas = square($_POST['name']);
+}
+
+$h1_text = "Atsakymas: $kvadratas";
 
 ?>
 <html>
@@ -29,6 +18,10 @@ var_dump($viematmas);
         <meta charset="UTF-8">
     </head>
     <body>
-
+        <form action="index.php" method="POST">
+            Ką pakelti kvadratu: <input type="number" name="name">
+            <input type="submit">
+        </form>
+        <h1><?php isset($h1_text) ? print $h1_text : print 'Nėra submit'; ?></h1>
     </body>
 </html>
