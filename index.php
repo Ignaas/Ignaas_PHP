e058970c2209294566a2ecc84ead2bf9ac71f9e9
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-09-30 12:52:02 +0300

    Užduotis #3

--- index.php
+++ index.php
@@ -16,6 +16,9 @@ if (isset($_POST['enter'])) {
     <body>
         <form method ="post">
             <input type="submit" name="enter" value="<?php print $clicks_num; ?>">
+            <?php for ($i = 0; $i < $clicks_num; $i++): ?>
+                <img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.kQQCJSSCFkfHkYsHmC6imgHaHT%26pid%3DApi&f=1" height="100" width="100"> 
+            <?php endfor; ?>
         </form>
     </body>
 </html>
