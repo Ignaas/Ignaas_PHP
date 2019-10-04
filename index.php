3f166835eb2f3f53dc6f84d87d2dbacdfc346627
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-10-04 15:59:19 +0300

    Užduotis #1 ir #2 ir #3 ir #H1 Patikrinimas ar netusčias Iškvietimas kiekvienos funkcijos, kuri yra masyve, pereina validaciją.

--- index.php
+++ index.php
@@ -18,7 +18,9 @@ $form = [
                 ],
             ],
             'label' => 'Vardas',
-//            'filter' => FILTER_SANITIZE_NUMBER_FLOAT,
+            'validate' => [
+                'validate_not_empty',
+            ],
         ],
         'lastname' => [
             'attr' => [
@@ -41,6 +43,23 @@ $form = [
                 'others' => 'Kiti',
             ],
             'label' => 'Dėmesio: pasirinkite lyti',
+        ],
+        'age' => [
+            'attr' => [
+                'type' => 'number',
+            ],
+            'extras' => [
+                'attr' => [
+                    'placeholder' => 'AGE',
+                ],
+            ],
+            'label' => 'Amžius',
+            'validate' => [
+                'validate_not_empty',
+                'validate_is_number',
+                'validate_is_positive',
+                'validate_max_100'
+            ],
         ],
     ],
     'buttons' => [
@@ -76,20 +95,54 @@ function get_filtered_input($form) {
     return filter_input_array(INPUT_POST, $filter_parameters);
 }
 
-$inputs = get_filtered_input($form);
-
-foreach ($form['fields'] as $field_id => &$field) {
-    $field['attr']['value'] = $inputs[$field_id];
-
-    if ($inputs[$field_id] === '') {
-        $field['error'] = 'Klaidos stringas';
+function validate_not_empty($field_input, &$field) {
+    if (isset($field_input)) {
+        if (empty($field_input)) {
+            $field['error'] = "Laukas negali būti tusčias";
+        }
     }
-
-    unset($field);
 }
 
-var_dump($_POST);
-var_dump($inputs);
+function validate_is_number($field_input, &$field) {
+    if (isset($field_input)) {
+        if (!is_numeric($field_input) && !empty($field_input)) {
+            $field['error'] = "Laukas negali nenumerinis";
+        }
+    }
+}
+
+function validate_is_positive($field_input, &$field) {
+    if (isset($field_input)) {
+        if ($field_input < 0) {
+            $field['error'] = "Laukas negali buti negatyvus";
+        }
+    }
+}
+
+function validate_max_100($field_input, &$field) {
+    if (isset($field_input)) {
+        if ($field_input >= 100) {
+            $field['error'] = "Laukas negali didesnis už 100";
+        }
+    }
+}
+
+function validate_form(&$form) {
+    $inputs = get_filtered_input($form);
+
+    foreach ($form['fields'] as $field_id => &$field) {
+        $field_input = $inputs[$field_id];
+        $field['attr']['value'] = $field_input;
+
+        foreach ($field['validate'] ?? [] as $validation) {
+            $validation($field_input, $field);
+        }
+    }
+
+    var_dump($inputs);
+}
+
+validate_form($form);
 
 ?>
 <html>
