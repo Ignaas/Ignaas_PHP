a775369886a890965a912b50675c2fb2bafaa7bf
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-10-09 10:54:45 +0300

    Užduotis #2 failu sutvarkymas

--- index.php
+++ index.php
@@ -26,7 +26,6 @@ $form = [
             'extra' => [
                 'attr' => [
                     'placeholder' => 'Enter your age',
-                    'class' => '',
                 ],
                 'validate' => [
                     'validate_is_number',
@@ -100,115 +99,24 @@ $form = [
     ],
 ];
 
-function validate_not_empty($field_input, &$field) {
-    if ($field_input === '') {
-        $field['error'] = 'Laukas negali būti tuščias.';
-    } else {
-        return true;
-    };
-}
-
-function validate_is_number($field_input, &$field) {
-    if (!is_numeric($field_input) && !empty($field_input)) {
-        $field['error'] = 'Lauke turi būti skaičius.';
-    } else {
-        return true;
-    };
-}
-
-function validate_is_positive($field_input, &$field) {
-    if ($field_input < 0) {
-        $field['error'] = 'Lauke turi būti skaičius, didesnis už 0.';
-    } else {
-        return true;
-    };
-}
-
-function validate_max_100($field_input, &$field) {
-    if ($field_input > 100) {
-        $field['error'] = 'Lauke turi būti skaičius, mažesnis nei 100.';
-    } else {
-        return true;
-    };
-}
-
-function validate_email($field_input, &$field) {
-    if (!strpos($field_input, '@') && !empty($field_input)) {
-        $field['error'] = 'Neteisingas emailas';
-    } else {
-        return true;
-    };
-}
-
-function form_success() {
-    if (!empty($_POST)) {
-        var_dump('success');
-    }
+function form_success($filtered_input) {
+    var_dump($filtered_input['number-x'] + $filtered_input['number-y']);
 }
 
 function form_fail() {
     var_dump('Klaida');
 }
 
-function html_attr($array) {
-    $container_array = [];
-    foreach ($array as $key => $value) {
-        $container_array[] = strtr('@key="@value"', [// @ skiriamasis zenklas
-            '@key' => $key,
-            '@value' => $value,
-        ]);
-    };
-    return implode(' ', $container_array);
-}
-
-function get_filtered_input($array) {
-    $filter_parameters = [];
-    foreach ($array['fields'] as $idx => $inner_array) {
-        $filter_parameters[$idx] = $inner_array['filter'] ?? FILTER_SANITIZE_SPECIAL_CHARS; // jei yra masivo filtras, ji naudoja, jei ne default sanitizas
-    };
-    return filter_input_array(INPUT_POST, $filter_parameters);
-}
+require './functions/html/generators.php';
+require './functions/form/core.php';
 
 $filtered_input = get_filtered_input($form);
 
-function validate_form($filtered_input, &$form) {
-    $success = true;
-    foreach ($form['fields'] as $field_id => &$field) {
-        $field_input = $filtered_input[$field_id];
-        $field['value'] = $field_input;
-        foreach ($field['extra']['validate'] ?? [] as $validator) {
-            $is_valid = $validator($field_input, $field); // kreipiamasi masyvo filtro masyvu kaip funkcijos vardu
-
-
-            if (!$is_valid) {
-                $success = false;
-                break;
-            }
-        }
-
-
-        unset($field);
-    }
-    //callbacks
-    if ($success) {
-        if (isset($form['callbacks']['success'])) {
-            $form['callbacks']['success']($filtered_input, $form);
-        }
-    } else {
-        if (isset($form['callbacks']['fail'])) {
-            $form['callbacks']['fail']($filtered_input, $form);
-        }
-    }
-    //callbacks
-    return $success;
-}
-
 if (!empty($filtered_input)) {
     $success = validate_form($filtered_input, $form);
-    if ($success) {
-        var_dump($filtered_input['number-x'] + $filtered_input['number-y']);
-    }
-};
+}
+
+var_dump($form);
 
 ?>
 <html>
