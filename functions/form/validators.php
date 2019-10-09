c8a29d5d03d9c6561a8792d60f74bebc1aa76e47
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-10-09 10:55:42 +0300

    Užduotis #2 failu sutvarkymas

---
+++ functions/form/validators.php
@@ -0,0 +1,41 @@
+<?php
+
+function validate_not_empty($field_input, &$field) {
+    if ($field_input === '') {
+        $field['error'] = 'Laukas negali būti tuščias.';
+    } else {
+        return true;
+    }
+}
+
+function validate_is_number($field_input, &$field) {
+    if (!is_numeric($field_input) && !empty($field_input)) {
+        $field['error'] = 'Lauke turi būti skaičius.';
+    } else {
+        return true;
+    }
+}
+
+function validate_is_positive($field_input, &$field) {
+    if ($field_input < 0) {
+        $field['error'] = 'Lauke turi būti skaičius, didesnis už 0.';
+    } else {
+        return true;
+    }
+}
+
+function validate_max_100($field_input, &$field) {
+    if ($field_input > 100) {
+        $field['error'] = 'Lauke turi būti skaičius, mažesnis nei 100.';
+    } else {
+        return true;
+    }
+}
+
+function validate_email($field_input, &$field) {
+    if (!strpos($field_input, '@') && !empty($field_input)) {
+        $field['error'] = 'Neteisingas emailas';
+    } else {
+        return true;
+    }
+}
